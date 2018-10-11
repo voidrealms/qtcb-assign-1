10e63c3f17c59850a2c662c11591b726940a4447
Bryan Cairns <bcairns@voidrealms.com>
2018-10-11 14:39:52 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //Nothing to see here - download and install Qt
+
+    return a.exec();
+}
